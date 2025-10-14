8315a6fb9315415e6935a65028dddd1bda536acb
Artyom22351 <132003976+Artyom22351@users.noreply.github.com>
2025-10-14 23:34:46 +0300

    1

--- start_bot.bat
+++ start_bot.bat
@@ -1,13 +1,1 @@
-@echo 
-title Telegram Game Privilege Bot
-echo ==========================================
-echo   
-echo ==========================================
-pip install --quiet --upgrade pip
-pip install aiogram python-dotenv mysql-connector
 
-echo.
-echo 
-echo 
-python main.py
-pause
